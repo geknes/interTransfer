b241ea4d14e91539446d5c8c2da7ecbad30cdda2
Jorge Luis Graza Melgarejo <jg.melg@gmail.com>
2018-12-24 14:15:17 -0500

    Initial commit

---
+++ src/test/java/com/hackmonkey/intertansfer/IntertansferApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hackmonkey.intertansfer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntertansferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
